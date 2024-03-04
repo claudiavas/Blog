2271a04755059c7d09a6a1bab517c838605c8154
Claudia Vasquez <claudia.vasquez.as@gmail.com>
2024-03-04 13:37:54 +0100

    updatle

--- next.config.js
+++ next.config.js
@@ -4,3 +4,61 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+// Webpack: digimatika/next.config.js
+
+// const path = require('path');
+
+// module.exports = {
+//   webpack: (config, { isServer }) => {
+//     // Agregar una regla personalizada para cargar archivos SVG
+//     config.module.rules.push({
+//       test: /\.svg$/,
+//       use: ['@svgr/webpack'],
+//     });
+
+//     return config;
+//   },
+// };
+
+// next.config.js
+const withMDX = require('@next/mdx')();
+
+module.exports = withMDX({
+  webpack: (config, { isServer }) => {
+    // Agregar una regla para procesar archivos Markdown
+    config.module.rules.push({
+      test: /\.md$/,
+      use: [
+        {
+          loader: 'markdown-loader',
+          options: {},
+        },
+      ],
+    });
+
+    return config;
+  },
+});
+
+const path = require('path');
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+    // Agregar una regla para procesar archivos Markdown
+    config.module.rules.push({
+      test: /\.md$/,
+      use: [
+        {
+          loader: 'raw-loader',
+        },
+        {
+          loader: 'markdown-loader',
+          options: {},
+        },
+      ],
+    });
+
+    return config;
+  },
+};
